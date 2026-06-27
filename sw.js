ba9957b08fa0221011c470880e1818cf84555bf7
aviadchai <aviadchai@gmail.com>
2026-06-27 17:19:41 +0300

    Bump cache v4, include PNG icons

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'apps-v3';
-const SHELL = ['./index.html', './shopping-list.html', './greek.html', './deal.html', './manifest.json', './greek-manifest.json', './deal-manifest.json', './icon.svg', './icon-shopping.svg', './icon-greek.svg', './icon-fitness.svg'];
+const CACHE = 'apps-v4';
+const SHELL = ['./index.html', './shopping-list.html', './greek.html', './deal.html', './manifest.json', './greek-manifest.json', './deal-manifest.json', './icon.svg', './icon-shopping.svg', './icon-greek.svg', './icon-fitness.svg', './icon-shopping-180.png', './icon-greek-180.png', './icon-fitness-180.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
