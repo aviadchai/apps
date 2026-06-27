25bd9b8a678bb2785d6e26df5c2b4b77efb94126
aviadchai <aviadchai@gmail.com>
2026-06-27 16:40:42 +0300

    Restructure as multi-app hub: new index, rename shopping-list, add manifests, update SW

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'shopping-v9';
-const SHELL = ['./index.html', './manifest.json', './icon.svg'];
+const CACHE = 'apps-v1';
+const SHELL = ['./index.html', './shopping-list.html', './greek.html', './deal.html', './manifest.json', './manifest-greek.json', './icon.svg'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
