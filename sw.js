70697bcea5ad5dd71edebbe796de49287ed4df91
aviadchai <aviadchai@gmail.com>
2026-06-28 17:41:39 +0300

    bump cache v10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'apps-v9';
+const CACHE = 'apps-v10';
 const SHELL = ['./index.html', './shopping-list.html', './greek.html', './greek-abc.html', './deal.html', './manifest.json', './greek-manifest.json', './greek-abc-manifest.json', './deal-manifest.json', './icon.svg', './icon-shopping.svg', './icon-greek.svg', './icon-fitness.svg', './icon-shopping-180.png', './icon-greek-180.png', './icon-fitness-180.png'];
 
 self.addEventListener('install', e => {
